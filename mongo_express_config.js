e62853452b17f198a38d0f37b655d5188c260cbc
rrrobo <rrrobo@icloud.com>
2021-10-02 15:01:17 +0900

    🔧mongo-express: enabled admin flag

--- mongo_express_config.js
+++ mongo_express_config.js
@@ -104,7 +104,7 @@ module.exports = {
     // set admin to true if you want to turn on admin features
     // if admin is true, the auth list below will be ignored
     // if admin is true, you will need to enter an admin username/password below (if it is needed)
-    admin: false,
+    admin: true,
 
     // whitelist: hide all databases except the ones in this list  (empty list for no whitelist)
     whitelist: [],
